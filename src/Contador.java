17a5629cfca4a050a6eb49fe0c152607b3d75d56
AndersonReiner <reiner.academico@gmail.com>
2024-05-31 23:23:18 -0400

    refactor: update finish

--- src/Contador.java
+++ src/Contador.java
@@ -40,12 +40,10 @@ public class Contador {
     }
 
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		if (parametroUm > parametroDois) {
             throw new  ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
         for(int i = 0; i < contagem; i ++){
             System.out.println("Imprimindo o número " + (i + 1));
         }
